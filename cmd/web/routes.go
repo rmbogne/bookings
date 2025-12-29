c145c3a96da5d3fa1c0e276c038ecf1369faa1c1
mbogne <r.mbogne@gmail.com>
2025-12-29 14:39:34 -0700

    static files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -20,8 +20,13 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Use(middleware.Recoverer)
 	mux.Use(NoSurf)
 	mux.Use(SessionLoad)
+
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	//Telling Go how and where to handle static files
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
